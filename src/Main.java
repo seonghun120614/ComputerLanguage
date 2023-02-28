49597fdf44b271d2b66789f5d113056ea96e4a8b
Park seonghun <seonghun120614@gmail.com>
2023-02-28 13:29:59 +0900

    week2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int hour = 3;
+        int minute = 3;
+        int totalMinute = hour * 60 + minute;
+        int numToBinary = 0b1100;
+        int numToOctal = 014;
+        int num = 12;
+
+        int numToHexadecimal = 0x0C;
+
+        float var = 3.15f;
+
+        System.out.println(hour + "h" + minute + "m");
+        System.out.println(hour * 60 + minute + "m");
+        System.out.println(totalMinute);
+        System.out.println(numToBinary);
+        System.out.println(numToOctal);
+        System.out.println(num);
+        System.out.println(numToHexadecimal);
     }
 }
